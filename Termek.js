953935355a5e4d0a8cbb463b8a489b465d0a3988
Petrovai Asztrik <asztrikpetrovai@gmail.com>
2023-05-05 09:27:55 +0200

    kedvencekhez rakás kész

--- Termek.js
+++ Termek.js
@@ -1,22 +1,35 @@
-import { KEDVENCLISTA} from "./adatok";
-
 class Termek {
+  #divElem;
+  #adat;
   constructor(ARTICLE, OBJ) {
-    ARTICLE.append(
-      `
-      <div>
-      <p> ${OBJ.termek} </p>
-      <p> ${OBJ.ar} </p>
-      <p> ${OBJ.szin} </p>
-      <button id="${i}">ELEM</button>
+    this.#adat = OBJ;
+    let txt = `
+      <div class="card col-lg-3 col-md-4 col-sm-6">
+      <h2 class="card-header">Termék 1</h2>
+      <p class="card-body"> ${this.#adat.termek} </p>
+      <p class="card-body"> ${this.#adat.ar} </p>
+      <p class="card-body"> ${this.#adat.szin} </p>
+      <div class="card-footer btn-group">
+      <button id="${this.#adat.id}">ELEM</button>
       </div>
-      `
-    );
+      </div>
+      `;
+    ARTICLE.append(txt);
+    console.log(txt);
 
-    $("button").on("click", function () {
-      i = this.id;
-      KEDVENCLISTA.append(i);
+    this.#divElem = ARTICLE.children("div:last-child");
+
+    this.#divElem.on("click", () => {
+      console.log(this.#adat.id)
+      this.esemenyTrigger();
     });
+  }
+  esemenyTrigger() {
+    console.log("trigger", this.#adat.id);
+    const esemeny = new CustomEvent("elemKattintas", {
+      detail: this.#adat.idje,
+    });
+    window.dispatchEvent(esemeny);
   }
 }
 export default Termek;
--- Termekek.js
+++ Termekek.js
@@ -1,13 +1,18 @@
 import Termek from "./Termek.js";
-import { LISTA } from "./adatok.js";
+import { KEDVENCLISTA, LISTA } from "./adatok.js";
 
 class Termekek {
+  #kedvencek = [];
   constructor() {
     const ARTICLE = $("article");
     for (let i = 0; i < LISTA.length; i++) {
-        const TERMEK = new Termek(ARTICLE, LISTA[i]);
-        
+      const TERMEK = new Termek(ARTICLE, LISTA[i], i);
     }
+
+    $(window).on("elemKattintas", (event) => {
+      KEDVENCLISTA.push(event.detail)
+      console.log(KEDVENCLISTA)
+    });
   }
 }
 export default Termekek;
